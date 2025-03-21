24d7b65d1824c9534aebf044843c1fe0b0ca6124
dpwl35 <dpwl9435@gmail.com>
2025-03-21 22:21:12 +0900

    feat: three.js 연동하기2

--- fastcampus/Part4/07_fancy-gallery/README.md
+++ fastcampus/Part4/07_fancy-gallery/README.md
@@ -20,4 +20,42 @@ const loadImages = async () => {
 
 three.js 에서 mesh를 만들 때  material에 텍스처 이미지를 정확히 입히기 위해서는 이미지가 로드된 상태여야 한다. (이미지의 위치, 높이, 이런 데이터를 정확히 가져와 만들 수 있음)  
 
-create 함수랑 initialize 함수도 async, await  수정 
+create 함수랑 initialize 함수도 async, await  수정  
+
+## 카메라 시야각 구하기
+이미지의 width, height값을 넘겨 줘도 mesh 크기가 커 보이는 이유 : width, height값을 줘도 카메라 위치가 canvas와 mesh가 가까우면 mesh가 더 커보인다.  
+  
+카메라의 위치와 시야각을 html사이즈와 동일하게 맞추는 작업이 필요.  
+
+camera.fov = ? 값 구하기  
+camera.position.set(0, 0, a); a = 카메라와 캔버스 사이의 거리   
+tan(theta) = canvas.height / 2 / a   
+arctan(canvas.height / 2 / 50 ) = theha  
+
+```javascript
+camera.fov = Math.atan(canvasSize.height / 2 / 50 ) * (180 / Math.PI) * 2;
+```
+const resize = () => {} 함수에도 넣어서 캔버스 사이즈가 바뀌어도 적용되게 함
+
+## mesh와 이미지 위치 맞추기
+mesh.position.y = top; 
+getBoundingClientRect 로 받아온 이미지 위치 정보 top, left값을 그대로 넣어주면 mesh위치가 다르게 보인다. three.js의 좌표체계와 html의 좌표체계가 달라서 생기는 일이다. 둘의 좌표체계를 맞춰주는 작업이 필요. 
+
+```javascript
+mesh.position.y = canvasSize.height / 2 - height / 2 - top;
+mesh.position.x = -canvasSize.width / 2 + width / 2 + left;
+```  
+three.js는 + 가 위로 올라가고 - 가 아래로 내려간다.  
+
+스크롤 이슈 > https://github.com/ashthornton/asscroll 사용  
+
+## canvas 넓이 따라 mesh scale 값 조정하기  
+```javascript
+const {width: originWidth} = mesh.geometry.parameters;
+const scale = width / originWidth; //넓이 갱신
+mesh.scale.x = scale;
+mesh.scale.y = scale;
+```
+
+
+
--- fastcampus/Part4/07_fancy-gallery/src/js/app.js
+++ fastcampus/Part4/07_fancy-gallery/src/js/app.js
@@ -1,6 +1,13 @@
 import * as THREE from "three";
 import vertexShader from '../shaders/vertex.glsl?raw';
 import fragmentShader from '../shaders/fragment.glsl?raw';
+import ASScroll from '@ashthornton/asscroll'
+
+const asscroll = new ASScroll({
+  disableRaf : true,
+});
+asscroll.enable();
+
 
 export default function () {
   const renderer = new THREE.WebGLRenderer({
@@ -23,8 +30,10 @@ export default function () {
     0.1,
     100
   );
-  camera.position.set(0, 0, 2);
+  camera.position.set(0, 0, 50);
+  camera.fov = Math.atan(canvasSize.height / 2 / 50 ) * (180 / Math.PI) * 2;
 
+  const imageRepository = [];
 
   const loadImages = async () => {
     const images = [...document.querySelectorAll('main .content img')];
@@ -45,8 +54,8 @@ export default function () {
 
     const imageMeshs = images.map(image => { //이미지 갯수만큼 생성 
 
-      const {width, height, top, left} = image.getBoundingClientRect(); //이미지 위치 정보 
-      console.log(width, height, top, left);
+      const {width, height} = image.getBoundingClientRect(); //이미지 위치 정보 
+      //console.log(width, height, top, left);
 
       const material = new THREE.ShaderMaterial({ 
         vertexShader: vertexShader,
@@ -56,6 +65,8 @@ export default function () {
   
       const geometry = new THREE.PlaneGeometry(width, height, 16, 16);
       const mesh = new THREE.Mesh(geometry, material);
+
+      imageRepository.push({img: image, mesh}) //이미지 정보 
 
       return mesh;
     });
@@ -75,18 +86,40 @@ export default function () {
     canvasSize.height = window.innerHeight;
 
     camera.aspect = canvasSize.width / canvasSize.height;
+    camera.fov = Math.atan(canvasSize.height / 2 / 50 ) * (180 / Math.PI) * 2; 
     camera.updateProjectionMatrix();
 
     renderer.setSize(canvasSize.width, canvasSize.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); 
   };
 
+  const retransform = () => {
+    imageRepository.forEach(({img, mesh}) => {
+      const {width, height, top, left} = img.getBoundingClientRect();
+      const {width: originWidth} = mesh.geometry.parameters;
+      // console.log(originWidth);
+      const scale = width / originWidth; //넓이 갱신
+      mesh.scale.x = scale;
+      mesh.scale.y = scale;
+
+      mesh.position.y = canvasSize.height / 2 - height / 2 - top;
+      mesh.position.x = -canvasSize.width / 2 + width / 2 + left;
+    })
+  }
+
   const addEvent = () => {
-    window.addEventListener("resize", resize);
+    window.addEventListener("resize", () => {
+      resize();
+      retransform(); 
+    });
   };
 
   const draw = () => {
     renderer.render(scene, camera);
+    retransform();
+
+    asscroll.update();
+
     requestAnimationFrame(() => {
       draw();
     });
