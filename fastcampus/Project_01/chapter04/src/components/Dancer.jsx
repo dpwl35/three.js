cd0f047e5ba3797e4bcbf86933194a91b0d48975
dpwl35 <dpwl9435@gmail.com>
2025-07-17 20:39:14 +1000

    feat:  gsap과 scrollControls 연동하여 Timeline 구성하기

--- fastcampus/Project_01/chapter04/src/components/Dancer.jsx
+++ fastcampus/Project_01/chapter04/src/components/Dancer.jsx
@@ -1,11 +1,15 @@
 import { useAnimations, useGLTF, useScroll } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { useRecoilValue } from "recoil";
 import { IsEnteredAtom } from "../stores";
 import { Loader } from "./Loader";
+import gsap from "gsap";
+
+let timeline;
 
 export const Dancer = () => {
+  const three = useThree();
   const isEntered = useRecoilValue(IsEnteredAtom);
   const dancerRef = useRef(null);
   const { scene, animations } = useGLTF("/models/dancer.glb");
@@ -16,6 +20,9 @@ export const Dancer = () => {
   const scroll = useScroll();
   useFrame(() => {
     console.log(scroll.offset);
+    if (!isEntered) return;
+    //타임라인제어
+    timeline.seek(scroll.offset * timeline.duration());
   });
 
   useEffect(() => {
@@ -23,6 +30,48 @@ export const Dancer = () => {
 
     actions["wave"].play();
   }, [actions, isEntered]);
+
+  useEffect(() => {
+    if (!isEntered) return;
+    if (!dancerRef.current) return;
+
+    gsap.fromTo(
+      three.camera.position,
+      {
+        x: -5,
+        y: 5,
+        z: 5,
+      },
+      {
+        duration: 2.5,
+        x: 0,
+        y: 6,
+        z: 12,
+      }
+    );
+    gsap.fromTo(
+      three.camera.rotation,
+      { z: Math.PI },
+      {
+        duration: 2.5,
+        z: 0,
+      }
+    );
+  }, [isEntered, three.camera.position, three.camera.rotation]);
+
+  useEffect(() => {
+    if (!isEntered) return;
+    if (!dancerRef.current) return;
+    timeline = gsap.timeline();
+    timeline.from(
+      dancerRef.current.rotation,
+      {
+        duration: 4,
+        y: -4 * Math.PI,
+      },
+      0.5
+    );
+  }, [isEntered]);
 
   if (isEntered) {
     return (
