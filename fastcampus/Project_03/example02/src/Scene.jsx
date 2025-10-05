59000a5f8c235503bf4425fbfaf6a976114c8cf8
dpwl35 <dpwl9435@gmail.com>
2025-10-05 21:44:39 +1100

    feat: 벽 고정체 만들기기

--- fastcampus/Project_03/example02/src/Scene.jsx
+++ fastcampus/Project_03/example02/src/Scene.jsx
@@ -4,12 +4,13 @@ import { Physics, Debug } from "@react-three/cannon";
 import Car from "./Car";
 import DummyBox from "./dummy/DummyBox";
 import DummyBall from "./dummy/DummyBall";
+import DummyWall from "./dummy/DummyWall";
 //import DummyMovementArea from "./dummy/DummyMovementArea";
 
 function Scene() {
   return (
     <>
-      <Canvas camera={{ fov: 45, position: [1.5, 2, 3] }}>
+      <Canvas camera={{ fov: 45, position: [1.5, 2, 5] }}>
         <color attach="background" />
         <ambientLight />
         <directionalLight position={[0, 5, 5]} />
@@ -19,6 +20,12 @@ function Scene() {
             <DummyBox position={[1, 0.2, -2]} args={[0.2, 0.2, 0.2]} />
             <DummyBall position={[0, 0.2, -2]} args={[0.15]} />
             {/* <DummyMovementArea position={[0, -0.2, 0]} /> */}
+
+            <DummyWall position={[5, 0.5, 0]} args={[1, 1, 10]} />
+            <DummyWall position={[0, 0.5, 5]} args={[10, 1, 1]} />
+            <DummyWall position={[0, 0.5, -5]} args={[10, 1, 1]} />
+            <DummyWall position={[-5, 0.5, 0]} args={[1, 1, 10]} />
+
             <Ground rotation={[-Math.PI / 2, 0, 0]} />
           </Debug>
         </Physics>
