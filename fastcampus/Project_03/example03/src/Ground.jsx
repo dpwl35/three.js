8a406a2ae02dad5a9a4f23f66a49934825958598
dpwl35 <dpwl9435@gmail.com>
2025-11-04 20:06:32 +1100

    feat: 공 glb 적용

--- fastcampus/Project_03/example03/src/Ground.jsx
+++ fastcampus/Project_03/example03/src/Ground.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React from "react";
 import { usePlane } from "@react-three/cannon";
+import DummyBall from "./dummy/DummyBall";
+import DummyBox from "./dummy/DummyBox";
+import DummyWall from "./dummy/DummyWall";
+import { Three } from "./components/Three";
+import { Ball } from "./components/Ball";
 
 export function Ground(props) {
-
-  const [meshRef] = usePlane(
-    () => ({ args: [15, 15], mass: 1, type: 'Static', ...props}),
-  )
+  const [meshRef] = usePlane(() => ({
+    args: [15, 15],
+    mass: 1,
+    type: "Static",
+    ...props,
+  }));
 
   return (
-    <mesh
-    {...props}
-    ref={meshRef} 
-    receiveShadow
-    >
-    <planeGeometry args={[15, 15]} />
-    <meshStandardMaterial color="white" opacity={0} transparent/>
-  </mesh>
-  )
+    <group>
+      <mesh {...props} ref={meshRef} receiveShadow>
+        <planeGeometry args={[15, 15]} />
+        <meshStandardMaterial color="white" opacity={0} transparent />
+      </mesh>
+
+      <Three position={[1, 0.5, -1]} />
+      <Three position={[-1, 0.5, -1]} />
+      <Three position={[3, 0.5, -1]} />
+      <Three position={[-3, 0.5, -1]} />
+
+      <Ball position={[2, 0.5, -1]} />
+
+      {/* <DummyBall position={[0, 0.2, -2]} args={[0.15]} /> */}
+      <DummyBox position={[1, 0.2, -2]} args={[0.2, 0.2, 0.2]} />
+      <DummyBox
+        position={[-1, 0.2, 1.5]}
+        args={[0.2, 0.4, 0.2]}
+        type={"Static"}
+      />
+      <DummyWall position={[5, 0.5, 0]} args={[1, 1, 10]} />
+      <DummyWall position={[0, 0.5, 5]} args={[10, 1, 1]} />
+      <DummyWall position={[0, 0.5, -5]} args={[10, 1, 1]} />
+      <DummyWall position={[-5, 0.5, 0]} args={[1, 1, 10]} />
+    </group>
+  );
 }
