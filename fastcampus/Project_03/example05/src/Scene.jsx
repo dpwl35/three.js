a9967e5f6f80808312c7f7ce71f1082f12e412f0
dpwl35 <dpwl9435@gmail.com>
2025-11-22 16:16:06 +1100

    feat: 폰트 최적화

--- fastcampus/Project_03/example05/src/Scene.jsx
+++ fastcampus/Project_03/example05/src/Scene.jsx
@@ -3,16 +3,42 @@ import { Ground } from "./Ground";
 import { Physics, Debug } from "@react-three/cannon";
 import Car from "./Car";
 import { useRecoilValue } from "recoil";
-import { useEffect } from "react";
+//import { useEffect } from "react";
 import { isStartScene } from "./utils/atom";
 import { Stats } from "@react-three/drei";
 import DrawCall from "./components/DrawCallCount";
+//import fontjson from "./Pretendard.json";
 
 function Scene() {
   const isStart = useRecoilValue(isStartScene);
-  useEffect(() => {
-    console.log(isStart);
-  }, [isStart]);
+
+  // useEffect(() => {
+  //   const fontData = fontjson;
+  //   console.log(fontData);
+  //   const targetText = "How to Play↑←↓→";
+  //   const modifiedGlyphs = {};
+
+  //   for (let i = 0; i < targetText.length; i++) {
+  //     const char = targetText[i];
+  //     const charKey = char in fontData.glyphs ? char : char.toUpperCase;
+
+  //     if (charKey in fontData.glyphs) {
+  //       modifiedGlyphs[charKey] = fontData.glyphs[charKey];
+  //     }
+  //   }
+
+  //   const modifiefontData = {
+  //     ...fontData,
+  //     glyphs: modifiedGlyphs,
+  //   };
+
+  //   console.log(JSON.stringify(modifiefontData));
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log(isStart);
+  // }, [isStart]);
+
   return (
     <>
       <Canvas camera={{ fov: 45, position: [1.5, 2, 4] }}>
