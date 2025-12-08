8169d9112ec50be892a077d9a25f225f5feceee4
dpwl35 <dpwl9435@gmail.com>
2025-12-08 23:24:11 +1100

    feat: 여러 유저의 상태 동기화 준비

--- fastcampus/Project_04/backend/server.js
+++ fastcampus/Project_04/backend/server.js
@@ -10,75 +10,78 @@ const io = new Server({
 io.listen(4000);
 
 const players = [];
+/*
+클라이언트의 소켓 아이디, 캐릭터의 위치 좌표, 유저 닉네임, 직군 정보, 캐릭터 모델링, 마이룸 배치 정보 
+*/
 
 io.on("connection", (socket) => {
   console.log("연결됨!");
 
-  // io.emit("players", players);
+  io.emit("players", players);
 
-  // socket.on(
-  //   "initialize",
-  //   ({ tempNickname, tempJobPosition, selectedCharacterGlbNameIndex }) => {
-  //     const newPlayer = {
-  //       id: socket.id,
-  //       position: [0, 0, 0],
-  //       nickname: tempNickname,
-  //       jobPosition: tempJobPosition,
-  //       selectedCharacterGlbNameIndex,
-  //       myRoom: {
-  //         objects: [],
-  //       },
-  //     };
-  //     players.push(newPlayer);
+  socket.on(
+    "initialize",
+    ({ tempNickname, tempJobPosition, selectedCharacterGlbNameIndex }) => {
+      const newPlayer = {
+        id: socket.id,
+        position: [0, 0, 0],
+        nickname: tempNickname,
+        jobPosition: tempJobPosition,
+        selectedCharacterGlbNameIndex,
+        myRoom: {
+          objects: [],
+        },
+      };
+      players.push(newPlayer);
 
-  //     socket.emit(
-  //       "initialize",
-  //       players.find((p) => p.id === socket.id)
-  //     );
-  //     io.emit("enter", {
-  //       id: socket.id,
-  //       nickname: newPlayer.nickname,
-  //       jobPosition: newPlayer.jobPosition,
-  //     });
-  //     io.emit("players", players);
-  //   }
-  // );
+      socket.emit(
+        "initialize",
+        players.find((p) => p.id === socket.id)
+      );
+      io.emit("enter", {
+        id: socket.id,
+        nickname: newPlayer.nickname,
+        jobPosition: newPlayer.jobPosition,
+      });
+      io.emit("players", players);
+    }
+  );
 
-  // socket.on("move", (position) => {
-  //   console.log("players", players);
-  //   const player = players.find((player) => player.id === socket.id);
-  //   if (player) {
-  //     player.position = position;
-  //     io.emit("players", players);
-  //   }
-  // });
+  socket.on("move", (position) => {
+    console.log("players", players);
+    const player = players.find((player) => player.id === socket.id);
+    if (player) {
+      player.position = position;
+      io.emit("players", players);
+    }
+  });
 
-  // socket.on("newText", (text) => {
-  //   const sender = players.find((player) => player.id === socket.id);
-  //   if (sender) {
-  //     const { id, nickname, jobPosition } = sender;
-  //     if (nickname && jobPosition) {
-  //       io.emit("newText", {
-  //         senderId: id,
-  //         senderNickname: nickname,
-  //         senderJobPosition: jobPosition,
-  //         text,
-  //         timestamp: new Date(),
-  //       });
-  //     }
-  //   }
-  // });
+  socket.on("newText", (text) => {
+    const sender = players.find((player) => player.id === socket.id);
+    if (sender) {
+      const { id, nickname, jobPosition } = sender;
+      if (nickname && jobPosition) {
+        io.emit("newText", {
+          senderId: id,
+          senderNickname: nickname,
+          senderJobPosition: jobPosition,
+          text,
+          timestamp: new Date(),
+        });
+      }
+    }
+  });
 
-  // socket.on("myRoomChange", (myRoom, otherPlayerId) => {
-  //   console.log("방이 바뀌었나?");
-  //   console.log("otherPlayerId", otherPlayerId);
-  //   console.log("socket.id", socket.id);
-  //   const id = otherPlayerId ?? socket.id;
-  //   const player = players.find((player) => player.id === id);
-  //   console.log("myRoom", myRoom);
-  //   player.myRoom = myRoom;
-  //   io.emit("players", players);
-  // });
+  socket.on("myRoomChange", (myRoom, otherPlayerId) => {
+    console.log("방이 바뀌었나?");
+    console.log("otherPlayerId", otherPlayerId);
+    console.log("socket.id", socket.id);
+    const id = otherPlayerId ?? socket.id;
+    const player = players.find((player) => player.id === id);
+    console.log("myRoom", myRoom);
+    player.myRoom = myRoom;
+    io.emit("players", players);
+  });
 
   socket.on("disconnecting", () => {
     console.log("연결이 끊어지는 중!");
